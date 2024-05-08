b42fd96e5101e9bdde46e05288b51efe0462c065
Ashish <ashish.kumar@primotech.com>
2024-05-08 15:38:59 +0530

    build files

--- src/redux/action/actionCreator.js
+++
@@ -1,3 +0,0 @@
-export const WEATHER_DATA = "WEATHER_DATA";
-export const SET_WEATHER_DATA = "SET_WEATHER_DATA";
-export const FETCH_ERROR = "FETCH_ERROR";
--- src/redux/reducer/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import weather from "./weather/index";
-
-const rootReducer = combineReducers({
-  weather
-});
-export default rootReducer;
